e5f10d7cc58b3e943c58f630a5617de6d87fb815
Sven Wiltink <swiltink@transip.nl>
2020-03-04 21:46:40 +0100

    added prolog

---
+++ 2019/prolog/01/main.pl
@@ -0,0 +1,30 @@
+#! /usr/bin/env swipl
+readInput(Input):-
+    setup_call_cleanup(
+        open("input.txt", read, Stream),
+        readData(Stream, Input),
+        close(Stream)
+    ).
+
+readData(Stream, Input):-
+    read_string(Stream, "\n", "\r", End, String),
+    (
+    (End == -1, String == "")
+    ->  Input = []
+    ;   Input = [String|Tail],
+        readData(Stream, Tail)
+    ).
+
+calculateMass([], 0).
+
+calculateMass([H|T], Mass):-
+    number_string(Hn, H),
+    Hm is floor(Hn/3) - 2,
+    Mass = Hm + N,
+    calculateMass(T, N).
+
+:-
+    readInput(Input),
+    calculateMass(Input, Mass),
+    format("Total mass: ~d\n", Mass),
+    halt.
