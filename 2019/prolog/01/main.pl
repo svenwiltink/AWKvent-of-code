707df562843e76e02220e26e61cc584f91eaf63f
Sven Wiltink <swiltink@transip.nl>
2020-03-05 22:12:07 +0100

    added d1p2

--- 2019/prolog/01/main.pl
+++ 2019/prolog/01/main.pl
@@ -15,16 +15,38 @@ readData(Stream, Input):-
         readData(Stream, Tail)
     ).
 
-calculateMass([], 0).
+p1CalculateMass([], 0).
 
-calculateMass([H|T], Mass):-
+p1CalculateMass([H|T], Mass):-
     number_string(Hn, H),
     Hm is floor(Hn/3) - 2,
-    calculateMass(T, N),
+    p1CalculateMass(T, N),
     Mass is Hm + N.
+
+% if the Mass is <= 0 we don't need extra fuel
+calculateExtraMass(M, E):-
+    M =< 8,
+    E is 0.
+
+calculateExtraMass(M, E):-
+    ExtraFuel is floor(M/3) - 2,
+    calculateExtraMass(ExtraFuel, ExtraFuelFuel),
+    E is ExtraFuel + ExtraFuelFuel.
+
+
+p2CalculateMass([], 0).
+
+p2CalculateMass([H|T], Mass):-
+    number_string(Hn, H),
+    Hm is floor(Hn/3) - 2,
+    calculateExtraMass(Hm, Hmextra),
+    p2CalculateMass(T, N),
+    Mass is Hm + Hmextra + N.
 
 :-
     readInput(Input),
-    calculateMass(Input, Mass),
-    format("Total mass: ~d\n", Mass),
+    p1CalculateMass(Input, Mass),
+    format("P1 Total mass: ~d\n", Mass),
+    p2CalculateMass(Input, P2Mass),
+    format("P2 Total mass: ~d\n", P2Mass),
     halt.
