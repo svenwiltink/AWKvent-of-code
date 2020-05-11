b667bc9c110bbcab11ec872897c1379a0bda44dc
Sven Wiltink <sven.wiltink@hotmail.com>
2020-05-11 19:11:34 +0200

    added prolog day3 part 2

--- 2019/prolog/03/main.pl
+++ 2019/prolog/03/main.pl
@@ -18,12 +18,14 @@ readLine(_, _, "", -1, _, _).
 readLine(_, SeriesName, String, -1, X, Y):-
         parseUnit(String, Direction, Amount),
         transformCoords(Direction, Amount, X, Y, NewX, NewY),
+        createEdge(SeriesName, X, Y, NewX, NewY),
         insertCoords(SeriesName, X, Y, NewX, NewY).
 
 % We've hit a newline. This value still belongs to the old series
 readLine(Stream, SeriesName, String, 10, X, Y):-
         parseUnit(String, Direction, Amount),
         transformCoords(Direction, Amount, X, Y, NewX, NewY),
+        createEdge(SeriesName, X, Y, NewX, NewY),
         insertCoords(SeriesName, X, Y, NewX, NewY),
         NSeriesName is SeriesName + 1,
         assert(coord(NSeriesName, 0, 0)),
@@ -33,6 +35,7 @@ readLine(Stream, SeriesName, String, 10, X, Y):-
 readLine(Stream, SeriesName, String, _, X, Y):-
         parseUnit(String, Direction, Amount),
         transformCoords(Direction, Amount, X, Y, NewX, NewY),
+        createEdge(SeriesName, X, Y, NewX, NewY),
         insertCoords(SeriesName, X, Y, NewX, NewY),
         readData(Stream, SeriesName, NewX, NewY).
 
@@ -41,6 +44,11 @@ parseUnit(Input, Direction, Amount):-
     sub_string(Input, 0, 1, After, Direction),
     sub_string(Input, 1, After, _, AmountString),
     number_string(Amount, AmountString).
+
+
+createEdge(SeriesName, X, Y, NewX, NewY):-
+    D is abs(NewX - X) + abs(NewY - Y),
+    assert(edge(SeriesName, X, Y, NewX, NewY, D)).
 
 % The coords are the same. We can stop stepping now
 insertCoords(Series, X, Y, X, Y):-
@@ -89,8 +97,50 @@ shortest(crossing(X, Y, D)):-
  crossing(X, Y, D),
  \+ (crossing(X2, Y2, D2), X \= X2, Y \= Y2, D2 < D).
 
+
+crossingBounds(MinX, MaxX, MinY, MaxY, X, Y):-
+    crossing(X, Y, _),
+    X >= MinX,
+    X =< MaxX,
+    Y >= MinY,
+    Y =< MaxY.
+
+% we found an edge that the crossing is on.
+distanceToCrossing(Series, CurrentX, CurrentY, CrossingX, CrossingY, Distance):-
+    edge(Series, CurrentX, CurrentY, NewX, NewY, _),
+    crossingBounds(
+        min(CurrentX, NewX),
+        max(CurrentX, NewX),
+        min(CurrentY, NewY),
+        max(CurrentY, NewY),
+        CrossingX, CrossingY),
+    Distance is abs(CrossingX - CurrentX) + abs(CrossingY - CurrentY).
+
+% Keep walking the path until we found an edge that the crossing is on
+distanceToCrossing(Series, CurrentX, CurrentY, CrossingX, CrossingY, Distance):-
+    edge(Series, CurrentX, CurrentY, NewX, NewY, D),
+    distanceToCrossing(Series, NewX, NewY, CrossingX, CrossingY, OtherDistance),
+    Distance is D + OtherDistance.
+
+
+combinedDistance(CrossingX, CrossingY, D):-
+    distanceToCrossing(0, 0, 0, CrossingX, CrossingY, D1),
+    distanceToCrossing(1, 0, 0, CrossingX, CrossingY, D2),
+    D is D1 + D2.
+
+closestCrossing(X, Y, D):-
+    crossing(X, Y, _),
+    combinedDistance(X, Y, D),
+    \+ (
+        crossing(X2, Y2, _),
+        combinedDistance(X2, Y2, D2),
+        D2 < D
+    ).
+
 :-
     readSeries,
-    shortest(crossing(_, _, D)),
-    format("Distance: ~p", D),
+    shortest(crossing(X, Y, D)),
+    format("Part 1: X ~p Y ~p Distance: ~p\n", [X, Y, D]),
+    closestCrossing(X2, Y2, D2),
+    format("Part 2: X ~p Y ~p Distance: ~p\n", [X2, Y2, D2]),
     halt.
