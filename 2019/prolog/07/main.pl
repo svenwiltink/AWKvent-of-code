6d9e89126046e0bc308ddd19bb95636551a42600
Sven Wiltink <sven.wiltink@hotmail.com>
2020-05-31 00:14:00 +0200

    semi working chaining

--- 2019/prolog/07/main.pl
+++ 2019/prolog/07/main.pl
@@ -35,7 +35,7 @@ getParameterMode(_, 1, Address, Value):-
 
 returnOnPrint.
 
-runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 1):-
+runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, 1):-
 
     L1 is PC + 1,
     L2 is PC + 2,
@@ -53,9 +53,9 @@ runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 1):-
 
     put_assoc(Pos3, IntCode, Number, NewIntCode),
 
-    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 2):-
+runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, 2):-
 
     L1 is PC + 1,
     L2 is PC + 2,
@@ -73,9 +73,9 @@ runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 2):-
 
     put_assoc(Pos3, IntCode, Number, NewIntCode),
 
-    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, _, [I|Inputs], Output, NPC, NIntCode, 3):-
+runIntCode(IntCode, PC, _, [I|Inputs], Output, NPC, NIntCode, NInputs, Halted, 3):-
     L1 is PC + 1,
 
     get_assoc(L1, IntCode, Pos1),
@@ -84,9 +84,9 @@ runIntCode(IntCode, PC, _, [I|Inputs], Output, NPC, NIntCode, 3):-
 
     PCn is PC + 2,
 
-    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, Mode, Inputs, [N|Output], NPC, NIntCode, 4):-
+runIntCode(IntCode, PC, Mode, Inputs, [N|Output], NPC, NIntCode, NInputs, Halted, 4):-
     L1 is PC + 1,
 
     get_assoc(L1, IntCode, Pos1),
@@ -94,14 +94,22 @@ runIntCode(IntCode, PC, Mode, Inputs, [N|Output], NPC, NIntCode, 4):-
 
     N is N1,
 
+%    format("Output ~p\n", N),
+
     PCn is PC + 2,
 
     ( returnOnPrint
-        -> (NPC is PCn, copy_term(IntCode, NIntCode))
-        ;  runIntCode(IntCode, PCn, Inputs, Output, NPC, NIntCode)
+        -> (
+            NPC is PCn,
+            copy_term(IntCode, NIntCode),
+            copy_term(NInputs, Inputs),
+            Halted is 0,
+            Output = []
+            )
+        ;  runIntCode(IntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted)
     ).
 
-runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 5):-
+runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, 5):-
     L1 is PC + 1,
     L2 is PC + 2,
 
@@ -116,9 +124,9 @@ runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 5):-
     ; PCn is PC + 3
     ),
 
-    runIntCode(IntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(IntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 6):-
+runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, 6):-
 
     L1 is PC + 1,
     L2 is PC + 2,
@@ -134,9 +142,9 @@ runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 6):-
     ; PCn is PC + 3
     ),
 
-    runIntCode(IntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(IntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 7):-
+runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, 7):-
 
     L1 is PC + 1,
     L2 is PC + 2,
@@ -158,9 +166,9 @@ runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 7):-
 
     put_assoc(Pos3, IntCode, Number, NewIntCode),
 
-    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 8):-
+runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, 8):-
 
     L1 is PC + 1,
     L2 is PC + 2,
@@ -182,17 +190,19 @@ runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, 8):-
 
     put_assoc(Pos3, IntCode, Number, NewIntCode),
 
-    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode).
+    runIntCode(NewIntCode, PCn, Inputs, Output, NPC, NIntCode, NInputs, Halted).
 
-runIntCode(IntCode, PC, _, _, _, NPC, NIntCode, 99):-
+runIntCode(IntCode, PC, _, Inputs, _, NPC, NIntCode, NInputs, Halted, 99):-
     NPC is PC,
+    copy_term(Inputs, NInputs),
+    Halted is 1,
     copy_term(IntCode, NIntCode).
 
-runIntCode(IntCode, PC, Inputs, Output, NPC, NIntCode):-
+runIntCode(IntCode, PC, Inputs, Output, NPC, NIntCode, NInputs, Halted):-
     get_assoc(PC, IntCode, OpCodeData),
     OpCode is mod(OpCodeData, 100),
     Mode is floor(OpCodeData / 100),
-    runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, OpCode).
+    runIntCode(IntCode, PC, Mode, Inputs, Output, NPC, NIntCode, NInputs, Halted, OpCode).
 
 runIntCodeWithReplacement(A, B, IntCode, Inputs, Value):-
     put_assoc(1, IntCode, A, IntCode2),
@@ -209,11 +219,11 @@ runAmps([PhaseA, PhaseB, PhaseC, PhaseD, PhaseE], OutputE):-
     copy_term(IntCode, AmpD),
     copy_term(IntCode, AmpE),
 
-    runIntCode(AmpA, 0, [PhaseA,0], [OutputA], _, _),
-    runIntCode(AmpB, 0, [PhaseB,OutputA], [OutputB], _, _),
-    runIntCode(AmpC, 0, [PhaseC,OutputB], [OutputC], _, _),
-    runIntCode(AmpD, 0, [PhaseD,OutputC], [OutputD], _, _),
-    runIntCode(AmpE, 0, [PhaseE,OutputD], [OutputE], _, _).
+    runIntCode(AmpA, 0, [PhaseA,0], [OutputA], _, _, _, _),
+    runIntCode(AmpB, 0, [PhaseB,OutputA], [OutputB], _, _, _, _),
+    runIntCode(AmpC, 0, [PhaseC,OutputB], [OutputC], _, _, _, _),
+    runIntCode(AmpD, 0, [PhaseD,OutputC], [OutputD], _, _, _, _),
+    runIntCode(AmpE, 0, [PhaseE,OutputD], [OutputE], _, _, _, _).
 
 findBestOutput(BestSetting, BestOutput):-
     permutation([0,1,2,3,4], BestSetting),
@@ -224,7 +234,60 @@ findBestOutput(BestSetting, BestOutput):-
         runAmps(OtherSetting, OtherOutput),
         OtherOutput > BestOutput
     ).
+
+startFeedbackAmps([PhaseA, PhaseB, PhaseC, PhaseD, PhaseE], Output):-
+        readIntcode(IntCode),
+        copy_term(IntCode, AmpA),
+        copy_term(IntCode, AmpB),
+        copy_term(IntCode, AmpC),
+        copy_term(IntCode, AmpD),
+        copy_term(IntCode, AmpE),
+
+        runFeedbackAmps([
+            amp("A", AmpA, 0, [PhaseA,0]),
+            amp("B", AmpB, 0, [PhaseB]),
+            amp("C", AmpC, 0, [PhaseC]),
+            amp("D", AmpD, 0, [PhaseD]),
+            amp("E", AmpE, 0,  [PhaseE])],
+            Output
+        ).
+
+findBestOutputPart2(BestOutput):-
+    permutation([5,6,7,8,9], BestSetting),
+    format("trying permutation ~p ~p ~p ~p ~p\n", BestSetting),
+    startFeedbackAmps(BestSetting, [BestOutput]),
+    format("Output ~p\n", [BestOutput]),
+
+    \+ (
+        permutation([5,6,7,8,9], OtherSetting),
+        format("trying other permutation ~p ~p ~p ~p ~p\n", OtherSetting),
+        startFeedbackAmps(OtherSetting, [OtherOutput]),
+        format("Otheroutput ~p\n", [OtherOutput]),
+
+        OtherOutput > BestOutput
+    ).
+
+amp(_, _, _, _).
+
+runFeedbackAmps(Amps, LastOutput):-
+    runFeedbackAmps(Amps, [], LastOutput).
+
+runFeedbackAmps([amp(N1, C1, P1, I1), amp(N2, C2, P2, I2), A3, A4, A5], Previous, LastOutput):-
+    runIntCode(C1, P1, I1, Output, NPC, NIntCode, NInputs, Halted),
+
+    (Halted = 1
+    ->  Output = [],
+        copy_term(Previous, LastOutput)
+    ;   append(I2, Output, NI2),
+        runFeedbackAmps([amp(N2, C2, P2, NI2), A3, A4, A5, amp(N1, NIntCode, NPC, NInputs)], Output, LastOutput)
+    ).
+
+%:-
+%    findBestOutput(_, Output),
+%    format("Part 1 ~p", Output),
+%    halt.
+
 :-
-    findBestOutput(_, Output),
-    format("Part 1 ~p", Output),
-    halt.
+    debug,
+    findBestOutputPart2(Output),
+    format("Part 2 ~p", Output).
