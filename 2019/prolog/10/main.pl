1d505fbf14f4978f124888d29c7a32dd96f56cbe
Sven Wiltink <sven.wiltink@hotmail.com>
2020-09-03 16:05:51 +0200

    Prolog day 10 part 1

--- 2019/prolog/10/main.pl
+++ 2019/prolog/10/main.pl
@@ -19,3 +19,44 @@ asteroid --> "#".
 :-
     phrase_from_file(lines(Asteroids, 0), 'input.txt'),
     forall(member(Fact, Asteroids), assertz(Fact)).
+
+polarCoord(X1, Y1, X2, Y2, Angle, Distance):-
+    Dx is X2 - X1,
+    Dy is Y2 - Y1,
+    Angle is atan2(Dy, Dx),
+    Distance is Dx**2 + Dy**2.
+
+los(X1, Y1, X2, Y2):-
+    location(X2, Y2, asteroid),
+    polarCoord(X1, Y1, X2, Y2, Angle, Distance),
+
+    \+ (
+        location(X3, Y3, asteroid),
+        polarCoord(X1, Y1, X3, Y3, Angle2, Distance2),
+        Angle = Angle2,
+        Distance2 < Distance
+    ).
+
+losCount(X, Y, C):-
+    location(X, Y, asteroid),
+    aggregate_all(count, los(X, Y, _, _), C).
+
+findBest([l(X, Y, C)|Rs], l(Cbx, Cby, Cbc), Best):-
+    (C > Cbc
+        ->  Nx is X,   Ny is Y,   Nc is C
+        ;   Nx is Cbx, Ny is Cby, Nc is Cbc
+    ),
+    findBest(Rs, l(Nx, Ny, Nc), Best).
+
+findBest([l(X, Y, C)|[]], l(Cbx, Cby, Cbc), Best):-
+        (C > Cbc
+            ->  Nx is X,   Ny is Y,   Nc is C
+            ;   Nx is Cbx, Ny is Cby, Nc is Cbc
+        ),
+        Best = l(Nx, Ny, Nc).
+
+:-
+    findall(l(X, Y, C), losCount(X, Y, C), B),
+    findBest(B, l(0,0,0), l(Xb, Yb, Cb)),
+    format("Part 1: X ~p Y ~p C ~p", [Xb, Yb, Cb]),
+    halt.
