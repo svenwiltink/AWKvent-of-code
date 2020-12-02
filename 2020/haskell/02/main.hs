804644ce0495236b12b6d3f03898e6db1aa17600
Sven Wiltink <swiltink@transip.nl>
2020-12-02 21:56:02 +0100

    haskell day 2 part II

--- 2020/haskell/02/main.hs
+++ 2020/haskell/02/main.hs
@@ -1,13 +1,23 @@
-
-validPassword :: String -> Bool
-validPassword x = do
-    let parts = words x
+parseLine :: String -> (Int, Int, Char, String)
+parseLine line = do
+    let parts = words line
     let min = read $ takeWhile (/='-') $ head parts :: Int
     let max = read $ tail . dropWhile (/='-') $ head parts :: Int
     let char = head $ takeWhile (/=':') $ parts !! 1
-    let num = length $ [y | y <- parts !! 2, y == char]
+    (min, max, char, parts !! 2)
+
+validPassword :: String -> Bool
+validPassword x = do
+    let (min, max, char, pass) = parseLine x
+    let num = length $ [y | y <- pass, y == char]
     num >= min && num <= max
+
+validPassword2 :: String -> Bool
+validPassword2 x = do
+    let (pos1, pos2, char, pass) = parseLine x
+    (if pass !! (pos1 - 1) == char then 1 else 0) + (if pass !! (pos2 - 1) == char then 1 else 0) == 1
 
 main = do
     content <- readFile "input.txt"
     print $ length $ [x | x <- lines content, validPassword x]
+    print $ length $ [x | x <- lines content, validPassword2 x]
