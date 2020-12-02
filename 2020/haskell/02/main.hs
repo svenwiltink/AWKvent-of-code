532ba77b83c69c69a1a4a42843a313824369cab6
Sven Wiltink <swiltink@transip.nl>
2020-12-02 21:37:57 +0100

    Haskell day 2 part I

---
+++ 2020/haskell/02/main.hs
@@ -0,0 +1,15 @@
+
+import Data.List.Split
+
+validPassword :: String -> Bool
+validPassword x = do
+    let parts = splitOn " " x
+    let min = read $ takeWhile (/='-') $ head parts :: Int
+    let max = read $ tail . dropWhile (/='-') $ head parts :: Int
+    let char = head $ takeWhile (/=':') $ parts !! 1
+    let num = length $ [y | y <- parts !! 2, y == char]
+    num >= min && num <= max
+
+main = do
+    content <- readFile "input.txt"
+    print $ length $ [x | x <- lines content, validPassword x]
