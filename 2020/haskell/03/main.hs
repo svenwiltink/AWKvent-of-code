a77c70d33de41ff0c1cdad0d1b515f95ae64f0a7
Sven Wiltink <swiltink@transip.nl>
2020-12-03 21:36:47 +0100

    2020 haskell day 3 part II

--- 2020/haskell/03/main.hs
+++ 2020/haskell/03/main.hs
@@ -1,6 +1,10 @@
-tree (x, slope) = hit
-    where hit = '#' == ( cycle slope !! (x * 3))
+tree :: (Int, String) -> Int -> Int -> Bool
+tree (x, slope) dX dY = '#' == ( cycle slope !! (x * dX `div` dY)) && x `mod` dY == 0
+
+trees :: String -> (Int, Int) -> Int
+trees input (dX, dY) = length $ [x | x <- zip [0,1..] . lines $ input, tree x dX dY]
 
 main = do
     content <- readFile "input.txt"
-    print $ length $ [x | x <- zip [0,1..] . lines $ content, tree x]
+    print $ trees content (3, 1)
+    print $ product $ map (trees content) [(1,1), (3,1), (5,1), (7,1), (1,2)]
