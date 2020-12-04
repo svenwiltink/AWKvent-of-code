e30edb2f7a2ee987722a2293cff04cd1323288e4
Sven Wiltink <swiltink@transip.nl>
2020-12-04 16:30:57 +0100

    minor cleanup

--- 2020/haskell/04/main.hs
+++ 2020/haskell/04/main.hs
@@ -13,7 +13,7 @@ validHeight height = (unit == "cm" && h >= 150 && h <= 193) || (unit == "in" && 
 validHairColour :: String -> Bool
 validHairColour colour = startOK && suffixOK
     where   startOK = head colour == '#'
-            suffixOK = (length (tail colour) == 6) && (length $ tail colour) == (length $ filter (`elem` "0123456789abcdef") (tail colour))
+            suffixOK = (length (tail colour) == 6) && length (tail colour) == length (filter (`elem` "0123456789abcdef") (tail colour))
 
 validProperty :: String -> String -> Bool
 validProperty "byr" year = x >= 1920 && x <= 2002 where x = read year::Int
@@ -28,8 +28,13 @@ validProperty "cid" _ = True
 validBlock :: String -> Bool
 validBlock i = validProperty (take 3 i) (drop 4 i)
 
+validPassword :: [String] -> Bool
+validPassword x = 
+    (length x == 8 && all validBlock x) || 
+    (length x == 7 && all validBlock x) && "cid" `notElem` map (take 3) x
+
 main = do
     content <- readFile "input.txt"
     let pass = passports $ lines content
     print $ length [x | x <- pass, (length x == 8) || (length x == 7) && "cid" `notElem` map (take 3) x]
-    print $ length [x | x <- pass, (length x == 8 && all validBlock x) || (length x == 7 && all validBlock x) && "cid" `notElem` map (take 3) x]
+    print $ length [x | x <- pass, validPassword x]
