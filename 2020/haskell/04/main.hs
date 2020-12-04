31af1910f4206e3cf21477cfb7387681598b55b9
Sven Wiltink <swiltink@transip.nl>
2020-12-04 16:37:36 +0100

    ore cleanup

--- 2020/haskell/04/main.hs
+++ 2020/haskell/04/main.hs
@@ -1,9 +1,8 @@
 passports :: [String] -> [[String]]
 passports [] = []
 passports x = pass : remaining
-    where 
-        pass = concatMap words $ takeWhile (/= "") x
-        remaining = passports (drop (length (takeWhile (/= "") x) + 1) x)
+    where   pass = concatMap words $ takeWhile (/= "") x
+            remaining = passports (drop (length (takeWhile (/= "") x) + 1) x)
 
 validHeight :: String -> Bool
 validHeight height = (unit == "cm" && h >= 150 && h <= 193) || (unit == "in" && h >= 59 && h <= 76)
@@ -13,7 +12,7 @@ validHeight height = (unit == "cm" && h >= 150 && h <= 193) || (unit == "in" && 
 validHairColour :: String -> Bool
 validHairColour colour = startOK && suffixOK
     where   startOK = head colour == '#'
-            suffixOK = (length (tail colour) == 6) && length (tail colour) == length (filter (`elem` "0123456789abcdef") (tail colour))
+            suffixOK = (length (tail colour) == 6) && all (`elem` ['0'..'9']++['a'..'f']) (tail colour)
 
 validProperty :: String -> String -> Bool
 validProperty "byr" year = x >= 1920 && x <= 2002 where x = read year::Int
@@ -22,19 +21,20 @@ validProperty "eyr" year = x >= 2020 && x <= 2030 where x = read year::Int
 validProperty "hgt" height = validHeight height
 validProperty "hcl" hc = validHairColour hc
 validProperty "ecl" ec = ec `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-validProperty "pid" pid = length pid == 9 && length (filter (`elem` "0123456789") pid) == 9
+validProperty "pid" pid = length pid == 9 && all (`elem` ['0'..'9']) pid
 validProperty "cid" _ = True
 
 validBlock :: String -> Bool
 validBlock i = validProperty (take 3 i) (drop 4 i)
 
-validPassword :: [String] -> Bool
-validPassword x = 
-    (length x == 8 && all validBlock x) || 
-    (length x == 7 && all validBlock x) && "cid" `notElem` map (take 3) x
+validPassport :: [String] -> Bool
+validPassport x = enoughFields x && all validBlock x
+
+enoughFields :: [String] -> Bool
+enoughFields x = (length x == 8) || (length x == 7 && "cid" `notElem` map (take 3) x)
 
 main = do
     content <- readFile "input.txt"
     let pass = passports $ lines content
-    print $ length [x | x <- pass, (length x == 8) || (length x == 7) && "cid" `notElem` map (take 3) x]
-    print $ length [x | x <- pass, validPassword x]
+    print $ length $ filter enoughFields pass
+    print $ length $ filter validPassport pass
