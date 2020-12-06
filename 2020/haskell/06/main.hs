f9057673c79c0e814a9246813f537e4a58c259fd
Sven Wiltink <swiltink@transip.nl>
2020-12-06 13:39:43 +0100

    2020 haskell day 6 part II

--- 2020/haskell/06/main.hs
+++ 2020/haskell/06/main.hs
@@ -1,4 +1,5 @@
 import Data.List
+import qualified Data.HashSet as HashSet
 
 answers :: [String] -> [[String]]
 answers [] = []
@@ -8,8 +9,14 @@ answers x = answer : remaining
 
 sameAnswer :: [String] -> Int
 sameAnswer [first] = length $ nub first
-sameAnswer (first:tail) = length $ nub $ concat a
-    where a = map (filter (`elem` first)) tail
+sameAnswer x = length $ intersectMultiple $ map HashSet.fromList x 
+        
+intersectMultiple :: [HashSet.HashSet Char] -> HashSet.HashSet Char
+intersectMultiple [x] = x
+intersectMultiple [x,t] = HashSet.intersection x t
+intersectMultiple (x:tail) = HashSet.intersection x remaining
+    where remaining = intersectMultiple tail
+
 main = do
     content <- readFile "input.txt"
     let a = answers $ lines content
