f6f22af054883af4d5ebe4c6681139c8528b653b
Sven Wiltink <swiltink@transip.nl>
2020-12-06 22:59:08 +0100

    use `span`

--- 2020/haskell/06/main.hs
+++ 2020/haskell/06/main.hs
@@ -1,11 +1,10 @@
-import Data.List
 import qualified Data.HashSet as HashSet
 
 answers :: [String] -> [[String]]
 answers [] = []
 answers x = answer : remaining
-    where   answer = takeWhile (/= "") x
-            remaining = answers $ drop (length answer + 1) x
+    where   (answer, r) = span (/= "") x
+            remaining = answers $ drop 1 r
 
 sameAnswer :: [String] -> Int
 sameAnswer x = length $ foldl1 HashSet.intersection (map HashSet.fromList x)
@@ -13,5 +12,5 @@ sameAnswer x = length $ foldl1 HashSet.intersection (map HashSet.fromList x)
 main = do
     content <- readFile "input.txt"
     let a = answers $ lines content
-    print $ sum $ map (length . nub . concat) a
+    print $ sum $ map (HashSet.size . HashSet.fromList . concat) a
     print $ sum $ map sameAnswer a
