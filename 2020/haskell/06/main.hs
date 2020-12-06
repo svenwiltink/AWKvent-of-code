a453c7bcceae334957d3c02dfda77ea507759dad
Sven Wiltink <swiltink@transip.nl>
2020-12-06 13:17:21 +0100

    broken part II

--- 2020/haskell/06/main.hs
+++ 2020/haskell/06/main.hs
@@ -1,12 +1,17 @@
 import Data.List
 
-answers :: [String] -> [[Char]]
+answers :: [String] -> [[String]]
 answers [] = []
 answers x = answer : remaining
-    where   answer = concat $ takeWhile (/= "") x
+    where   answer = takeWhile (/= "") x
             remaining = answers (drop (length (takeWhile (/= "") x) + 1) x)
 
+sameAnswer :: [String] -> Int
+sameAnswer [first] = length $ nub first
+sameAnswer (first:tail) = length $ nub $ concat a
+    where a = map (filter (`elem` first)) tail
 main = do
     content <- readFile "input.txt"
     let a = answers $ lines content
-    print $ sum $ map (length . nub) a
+    print $ sum $ map (length . nub . concat) a
+    print $ sum $ map sameAnswer a
