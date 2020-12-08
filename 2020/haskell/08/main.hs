63a77330aa803d045ae1021020d353ba2a361493
Sven Wiltink <swiltink@transip.nl>
2020-12-08 21:44:38 +0100

    Haskell day 8 part II

--- 2020/haskell/08/main.hs
+++ 2020/haskell/08/main.hs
@@ -9,7 +9,8 @@ data Instruction = Instruction
 data Machine = Machine
   { pc :: Int,
     acc :: Int,
-    instructions :: [Instruction]
+    instructions :: [Instruction],
+    replacement :: Int
   }
   deriving (Show)
 
@@ -21,7 +22,7 @@ parseInstruction x = Instruction {name = name, parameters = parameters}
     parameters = tail w
 
 parseMachineCode :: String -> Machine
-parseMachineCode x = Machine {pc = 0, acc = 0, instructions = instructions}
+parseMachineCode x = Machine {pc = 0, acc = 0, replacement = -1, instructions = instructions}
   where
     instructions = map parseInstruction $ lines x
 
@@ -31,14 +32,24 @@ runMachineUntilLoop = runUntilVisited Set.empty
 runUntilVisited :: Set.HashSet Int -> Machine -> Machine
 runUntilVisited visited m
   | visitedBefore = m
+  | programEnded = m
   | otherwise = runUntilVisited (Set.insert (pc m) visited) $ stepMachine m
   where
     visitedBefore = Set.member (pc m) visited
+    programEnded = pc m >= length (instructions m)
 
 stepMachine :: Machine -> Machine
-stepMachine m = runInstruction i m
+stepMachine m
+  | replace == cpc = runInstruction (replaceInstruction i) m
+  | otherwise = runInstruction i m
   where
+    cpc = pc m
+    replace = replacement m
     i = instructions m !! pc m
+
+replaceInstruction :: Instruction -> Instruction
+replaceInstruction i@Instruction {name = "nop"} = i {name = "jmp"}
+replaceInstruction i@Instruction {name = "jmp"} = i {name = "nop"}
 
 runInstruction :: Instruction -> Machine -> Machine
 runInstruction Instruction {name = "nop"} m = m {pc = pc m + 1}
@@ -54,7 +65,27 @@ signedInt :: String -> Int
 signedInt ('+' : i) = read i
 signedInt i = read i :: Int
 
+findCorrectReplacement :: Machine -> Machine
+findCorrectReplacement m = snd $ head matching
+    where   machines = zip (repeat m) [0 .. (length (instructions m) -1)]
+            results = map (uncurry tryReplacement) machines
+            matching = filter fst results
+
+tryReplacement :: Machine -> Int -> (Bool, Machine)
+tryReplacement m inst
+  | opcode `elem` ["nop", "jmp"] = runReplacement m inst
+  | otherwise = (False, m) -- never try this variant. It will never succeed
+  where
+    opcode = name $ instructions m !! inst
+
+runReplacement :: Machine -> Int -> (Bool, Machine)
+runReplacement m i = (ok, finishedMachine)
+  where
+    finishedMachine = runMachineUntilLoop m {replacement = i}
+    ok = pc finishedMachine == length (instructions m)
+
 main = do
   content <- readFile "input.txt"
   let machine = parseMachineCode content
   print $ acc $ runMachineUntilLoop machine
+  print $ acc $ findCorrectReplacement machine
