c3513a0e2958a011bc027434e94c327238007818
Sven Wiltink <swiltink@transip.nl>
2020-12-09 19:54:40 +0100

    2020 haskell day 9 part II

--- 2020/haskell/09/main.hs
+++ 2020/haskell/09/main.hs
@@ -1,17 +1,38 @@
+import Data.Maybe
+
 parse :: String -> [Int]
 parse x = map read $ lines x
 
 valid :: [Int] -> Int -> Int -> Bool
 valid input preamble index = not (null matching)
-    where numbers = take preamble $ drop (index-preamble) input
-          number = input !! index
-          matching = [(x,y) | x <- numbers, y <- numbers, x /= y, x + y == number]
+  where
+    numbers = take preamble $ drop (index - preamble) input
+    number = input !! index
+    matching = [(x, y) | x <- numbers, y <- numbers, x /= y, x + y == number]
 
 firstInvalid :: [Int] -> Int -> Int
 firstInvalid input preamble = input !! head invalid
-    where invalid = filter (not . valid input preamble) [preamble..(length input -1)]
+  where
+    invalid = filter (not . valid input preamble) [preamble .. (length input -1)]
+
+checkRange :: [Int] -> Int -> Int -> Maybe [Int]
+checkRange input goal start
+  | not (null range) && last range == goal = Just $ take (length range) remainder
+  | otherwise = Nothing
+  where
+    remainder = drop start input
+    range = takeWhile (<= goal) $ scanl1 (+) remainder
+
+findRange :: [Int] -> Int -> [Int]
+findRange input target = fromJust $ head correct
+  where
+    checked = map (checkRange input target) [0 .. length input -1]
+    correct = filter (\x -> isJust x && length (fromJust x) > 1) checked
 
 main = do
-    content <- readFile "input.txt"
-    let num = parse content
-    print $ firstInvalid num 25
+  content <- readFile "input.txt"
+  let num = parse content
+  let invalid = firstInvalid num 25
+  print invalid
+  let range = findRange num invalid
+  print $ minimum range + maximum range
