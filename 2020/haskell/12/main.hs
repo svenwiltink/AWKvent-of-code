27a1e850665235d4bb748191252bf01617651162
Sven Wiltink <swiltink@transip.nl>
2020-12-13 00:14:20 +0100

    2020 haskell day 12 II

--- 2020/haskell/12/main.hs
+++ 2020/haskell/12/main.hs
@@ -1,21 +1,53 @@
-data Ferry = Ferry { angle :: Int, x :: Int, y :: Int} deriving (Show)
+data Ferry = Ferry {angle :: Int, x :: Int, y :: Int, wx :: Int, wy :: Int} deriving (Show)
 
-run :: Ferry -> [String] -> Ferry
-run = foldl runInstrunction
+runOne :: Ferry -> [String] -> Ferry
+runOne = foldl runInstrunctionOne
 
-runInstrunction :: Ferry -> String -> Ferry
-runInstrunction f ('N' : n) = f {y = newY} where newY = y f + read n
-runInstrunction f ('S' : n) = f {y = newY} where newY = y f - read n
-runInstrunction f ('E' : n) = f {x = newX} where newX = x f + read n
-runInstrunction f ('W' : n) = f {x = newX} where newX = x f - read n
-runInstrunction f ('R' : n) = f {angle = newA} where newA = (angle f + read n + 360) `mod` 360
-runInstrunction f ('L' : n) = f {angle = newA} where newA = (angle f - read n + 360) `mod` 360
-runInstrunction f@Ferry {angle = 0} ('F' : n) = f {y = newY} where newY = y f + read n
-runInstrunction f@Ferry {angle = 90} ('F' : n) = f {x = newX} where newX = x f + read n
-runInstrunction f@Ferry {angle = 180} ('F' : n) = f {y = newY} where newY = y f - read n
-runInstrunction f@Ferry {angle = 270} ('F' : n) = f {x = newX} where newX = x f - read n
+runInstrunctionOne :: Ferry -> String -> Ferry
+runInstrunctionOne f ('N' : n) = f {y = newY} where newY = y f + read n
+runInstrunctionOne f ('S' : n) = f {y = newY} where newY = y f - read n
+runInstrunctionOne f ('E' : n) = f {x = newX} where newX = x f + read n
+runInstrunctionOne f ('W' : n) = f {x = newX} where newX = x f - read n
+runInstrunctionOne f ('R' : n) = f {angle = newA} where newA = (angle f + read n + 360) `mod` 360
+runInstrunctionOne f ('L' : n) = f {angle = newA} where newA = (angle f - read n + 360) `mod` 360
+runInstrunctionOne f@Ferry {angle = 0} ('F' : n) = f {y = newY} where newY = y f + read n
+runInstrunctionOne f@Ferry {angle = 90} ('F' : n) = f {x = newX} where newX = x f + read n
+runInstrunctionOne f@Ferry {angle = 180} ('F' : n) = f {y = newY} where newY = y f - read n
+runInstrunctionOne f@Ferry {angle = 270} ('F' : n) = f {x = newX} where newX = x f - read n
+
+runTwo :: Ferry -> [String] -> Ferry
+runTwo = foldl runInstrunctionTwo
+
+runInstrunctionTwo :: Ferry -> String -> Ferry
+runInstrunctionTwo f ('N' : n) = f {wy = newY} where newY = wy f + read n
+runInstrunctionTwo f ('S' : n) = f {wy = newY} where newY = wy f - read n
+runInstrunctionTwo f ('E' : n) = f {wx = newX} where newX = wx f + read n
+runInstrunctionTwo f ('W' : n) = f {wx = newX} where newX = wx f - read n
+
+runInstrunctionTwo f ('R' : "90") = f {wx=newX, wy=newY} 
+    where newX = wy f
+          newY = -wx f
+
+runInstrunctionTwo f ('R' : "180") = f {wx=newX, wy=newY} 
+    where newX = -wx f
+          newY = -wy f
+
+runInstrunctionTwo f ('R' : "270") = f {wx=newX, wy=newY} 
+    where newX = -wy f
+          newY = wx f
+
+runInstrunctionTwo f ('L' : "90") = runInstrunctionTwo f "R270"
+runInstrunctionTwo f ('L' : "180") = runInstrunctionTwo f "R180"
+runInstrunctionTwo f ('L' : "270") = runInstrunctionTwo f "R90"
+
+runInstrunctionTwo f('F' : n) = f {x = newX, y = newY}
+    where m = read n :: Int
+          newX = x f + wx f * m
+          newY = y f + wy f * m
 
 main = do
   content <- readFile "input.txt"
-  let f = run Ferry {angle = 90, x = 0, y = 0} $ lines content
+  let f = runOne Ferry {angle = 90, x = 0, y = 0, wx = 0, wy = 0} $ lines content
   print $ abs (x f) + abs (y f)
+  let f2 = runTwo Ferry {angle = 90, x = 0, y = 0, wx = 10, wy = 1} $ lines content
+  print $ abs (x f2) + abs (y f2)
